6f70aba4450615383e967b2e1aa9df5c8f0d0cd3
Faqihazh <faqihazh@gmail.com>
2025-07-27 22:34:11 +0700

    refactor project structure

--- config/db.js
+++
@@ -1,6 +0,0 @@
-import postgres from "postgres";
-import "dotenv/config";
-
-export const sql = postgres(process.env.DATABASE_URL, {
-  ssl: false,
-});
---
+++ src/config/db.js
@@ -0,0 +1,24 @@
+import postgres from "postgres";
+import "dotenv/config";
+
+export const sql = postgres(process.env.DATABASE_URL, {
+  ssl: false,
+});
+
+export async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions (
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      amount DECIMAL(10, 2) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      CREATED_AT DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initialized");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+    process.exit(1);
+  }
+}
